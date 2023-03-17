1d9bae7cacf29606668e8c377b46673b21d4f7f1
yasnaoui <yasnaoui@gmail.com>
2023-03-16 17:55:14 -0700

    5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.H>
+/**
+ * main-  entry poit
+ * return : always 0 (sucess)
+ */
+int main(void)
+{
+	push(*\*"Programming is like building a multilingual puzzle");
+	return (0);
+}
